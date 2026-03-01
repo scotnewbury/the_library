5bcf0b1f21729c1201a35657200b22762787095b
Scot Newbury <scot@scotnewbury.com>
2026-03-01 13:01:09 -0500

    Added a loop with an exit clause for persistent usage

--- parser.php
+++ parser.php
@@ -1,14 +1,23 @@
 <?php
 
+// Instruction block
+echo "Welcome to The Library!" . PHP_EOL;
+echo "To add a new book, please enter the 13 digit ISBN number." . PHP_EOL . PHP_EOL;
+echo "To exit this applicaiton, enter the word 'exit' instead of a number." . PHP_EOL;
+
 // Grab the user input
 $userInput = readline("Enter the ISBN: ");
 
-// Remove everything that isn't a number
-$cleanedInput = preg_replace('/[^0-9]/', '', $userInput);
+while ($userInput != "exit") {
+  // Remove everything that isn't a number
+  $cleanedInput = preg_replace('/[^0-9]/', '', $userInput);
 
-// Check to make sure we have 13 digits left - the size of an ISBN
-if (strlen($cleanedInput) != 13) {
-    echo "ISBN numbers are 13 digits, please reenter your number." . PHP_EOL;
-} else {
-    echo "You entered " . $cleanedInput . PHP_EOL;
+  // Check to make sure we have 13 digits left - the size of an ISBN
+  if (strlen($cleanedInput) != 13) {
+      echo "Please remember, ISBN numbers are 13 digit numbers, please reenter your number." . PHP_EOL;
+  } else {
+      echo "You entered " . $cleanedInput . PHP_EOL;
+  }
+  // Grab the user input
+  $userInput = readline("Enter the ISBN (or exit to quit): ");
 }
