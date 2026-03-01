9d865412bd188af432021c85dbdcfa8bbb316f22
Scot Newbury <scot@scotnewbury.com>
2026-03-01 12:47:07 -0500

    Added commets for clarity

--- parser.php
+++ parser.php
@@ -1,6 +1,12 @@
 <?php
+
+// Grab the user input
 $userInput = readline("Enter the ISBN: ");
+
+// Remove everything that isn't a number
 $cleanedInput = preg_replace('/[^0-9]/', '', $userInput);
+
+// Check to make sure we have 13 digits left - the size of an ISBN
 if (strlen($cleanedInput) != 13) {
     echo "ISBN numbers are 13 digits, please reenter your number." . PHP_EOL;
 } else {
