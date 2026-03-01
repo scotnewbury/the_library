915361f8524fdf1689f87eb48a67eb5862fa742b
Scot Newbury <scot@scotnewbury.com>
2026-03-01 16:13:02 -0500

    Updated to include logic to make sure the file can be written to

--- write_to_book_ledger.php
+++ write_to_book_ledger.php
@@ -6,6 +6,9 @@
 function writeToBookLedger($isbn) {
   $currentTimestamp = date('Y-m-d H:i:s', time());
   $handle = fopen("books.csv", "a");
+  if ($handle === false) {
+    die("Error: Unable to open the Book Ledger for writing.");
+  }
   fputcsv($handle, [$isbn, $currentTimestamp]);
   fclose($handle);
 }
