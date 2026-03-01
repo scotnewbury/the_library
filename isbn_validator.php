d1fe081a4e496808e1b96bdeba81f36a82a1ddd3
Scot Newbury <scot@scotnewbury.com>
2026-03-01 14:21:11 -0500

    Add to include function to check validity of ISBN number - Modulo 10

---
+++ isbn_validator.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * isbn_validator.php
+ * Contains logic for validating ISBN numbers.
+ */
+
+function isValidIsbn13($isbn) {
+    if (strlen($isbn) !== 13) return false;
+
+    $sum = 0;
+    for ($i = 0; $i < 12; $i++) {
+        $sum += (int)$isbn[$i] * ($i % 2 === 0 ? 1 : 3);
+    }
+
+    $checkDigit = (10 - ($sum % 10)) % 10;
+    return $checkDigit === (int)$isbn[12];
+}
