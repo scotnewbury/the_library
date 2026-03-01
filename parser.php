a00ad5097e4e206bb855ac5ea1795dc20c4e2a18
Scot Newbury <scot@scotnewbury.com>
2026-03-01 14:00:16 -0500

    Updated loop and process logic, added trim function

--- parser.php
+++ parser.php
@@ -5,19 +5,25 @@ echo "Welcome to The Library!" . PHP_EOL;
 echo "To add a new book, please enter the 13 digit ISBN number." . PHP_EOL . PHP_EOL;
 echo "To exit this applicaiton, enter the word 'exit' instead of a number." . PHP_EOL;
 
-// Grab the user input
-$userInput = readline("Enter the ISBN: ");
-
-while ($userInput != "exit") {
-  // Remove everything that isn't a number
-  $cleanedInput = preg_replace('/[^0-9]/', '', $userInput);
-
-  // Check to make sure we have 13 digits left - the size of an ISBN
-  if (strlen($cleanedInput) != 13) {
-      echo "Please remember, ISBN numbers are 13 digit numbers, please reenter your number." . PHP_EOL;
-  } else {
-      echo "You entered " . $cleanedInput . PHP_EOL;
-  }
+while (true) {
   // Grab the user input
   $userInput = readline("Enter the ISBN (or exit to quit): ");
+
+  // Convert the input to lowercase and trim for the 'exit' check
+  $normalizedInput = strtolower(trim($userInput));
+  // Check to see if the user wants to exit
+  if ($normalizedInput == "exit") {
+    break;
+  }
+    
+  // Remove everything that isn't a number
+  $cleanedInput = preg_replace('/[^0-9]/', '', $normalizedInput);
+  $inputLength = strlen($cleanedInput);
+
+  // Check to make sure we have 13 digits left - the size of an ISBN
+  if ($inputLength === 13) {
+    echo "You entered " . $cleanedInput . PHP_EOL;echo "You entered " . $cleanedInput . PHP_EOL;    
+  } else {
+    echo "Please remember, ISBN numbers are 13 digit numbers, please reenter your number." . PHP_EOL;
+  }
 }
