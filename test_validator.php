3189f56cdf50a6cecfb5baa515fcc3cf89c18d5a
Scot Newbury <scot@scotnewbury.com>
2026-03-02 14:06:45 -0500

    First version of test script with all test conditions

--- test_validator.php
+++ test_validator.php
@@ -4,16 +4,22 @@ require_once 'isbn_validator.php';
 
 $test_conditions = [
   "9781603020220" => "Valid ISBN13",
+  "123" => "Incorrect Length",
+  "abc456" => "Incorrect Length",
+  "abcdef" => "Incorrect Length",
+  "" => "Incorrect Length",
+  "12345678901234" => "Incorrect Length",
+  "1234567890123" => "Checksum Incorrect"
 ];
 
 foreach ($test_conditions as $key => $value) {
-    echo "$key\n";
-    echo "$value\n";
+    // echo "$key\n";
+    // echo "$value\n";
     $test_response = isValidIsbn13($key);
 
     if ($test_response == $value) {
-      echo "Value is correct";
+      echo "Key: " . $key . " - " . $value . " PASS" . PHP_EOL;
     } else {
-      echo "Value is incorrect";
+      echo "Key: " . $key . " - " . $value . " FAIL" . PHP_EOL;
     }
 }
