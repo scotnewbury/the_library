a1197a05123fbf52f60d862ccccf36da3a81ba1a
Scot Newbury <scot@scotnewbury.com>
2026-03-03 18:23:36 -0500

    Removed testing statements and updated comments

--- write_to_book_ledger.php
+++ write_to_book_ledger.php
@@ -4,17 +4,17 @@
  * Writes to the book.csv file the ISBN and current timestamp
  */
 function writeToBookLedger($bookInfo) {
-  echo "Here I am!!!" . PHP_EOL;
   $currentTimestamp = date('Y-m-d H:i:s', time());
   $handle = fopen("books.csv", "a");
   if ($handle === false) {
     die("Error: Unable to open the Book Ledger for writing.");
   }
-  // fputcsv($handle, [$isbn, $currentTimestamp]);
   
+  // Pull just the array values out
   $rowData = array_values($bookInfo);
+  // Add the current timestamp to the end of the row
   $rowData[] = $currentTimestamp;
+  // Write the row
   fputcsv($handle, $rowData);
-  echo "Writing to the ledger" . PHP_EOL;
   fclose($handle);
 }
