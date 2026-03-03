cfccc7a2307fd3dde6750d4c6590b86d93d0fdb8
Scot Newbury <scot@scotnewbury.com>
2026-03-03 17:55:14 -0500

    Coverted code to a callalbe function.

--- get_book_metadata.php
+++ get_book_metadata.php
@@ -1,26 +1,36 @@
 <?php
 
+function getBookMetadata ($isbn) {
+  // Create the URL for the API call
+  $url = "http://openlibrary.org/search.json?q=". $isbn;
+
+  // Grab the json from the Open Library API
+  $jsonData = file_get_contents($url);
+
+  // Did we get json data from the call?
+  if ($jsonData === false || empty($jsonData)) {
+    die("Error: Could not fetch data from the API.");
+  }
+
+  // // Conver the data to an associative array
+  $bookData = json_decode($jsonData, true);
+
+  // Check that we received book information
+  if ($bookData['numFound'] > 0) {
+      $book = $bookData['docs'][0];
+      
+      // Now you can access the 'data' key which contains the title
+      $title = $book['title'];
+      return [
+        'title' => $title,
+      ];
+  } else {
+      echo "No records found for this ISBN.";
+  } 
+}
+
 $isbn = "9781603020220";
-$url = "http://openlibrary.org/search.json?q=". $isbn;
 
-echo $url . PHP_EOL;
+$bookInfo = getBookMetadata($isbn);
 
-// Grab the json from the Open Library API
-$jsonData = file_get_contents($url);
-
-// // Conver the data to an associative array
-$bookData = json_decode($jsonData, true);
-
-// Check that we received book information
-if ($bookData['numFound'] > 0) {
-    $book = $bookData['docs'][0];
-    
-    // Now you can access the 'data' key which contains the title
-    $title = $book['title'];
-} else {
-    echo "No records found for this ISBN.";
-} 
-
-echo $bookData['docs']['0']['title'] . PHP_EOL;
-
-// https://openlibrary.org/search.json?q=9781603020220
+echo "The book title is: " . $bookInfo['title'] . PHP_EOL;
