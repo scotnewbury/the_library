437d5966bf54ddbb658f9270d8aa4629971ac22d
Scot Newbury <scot@scotnewbury.com>
2026-03-03 17:45:53 -0500

    Updated to use Open Library Search API and completed successful tests.

--- get_book_metadata.php
+++ get_book_metadata.php
@@ -1,13 +1,26 @@
 <?php
 
 $isbn = "9781603020220";
-
-$url = "http://openlibrary.org/api/volumes/brief/isbn/". $isbn . ".json";
+$url = "http://openlibrary.org/search.json?q=". $isbn;
 
 echo $url . PHP_EOL;
 
+// Grab the json from the Open Library API
 $jsonData = file_get_contents($url);
 
+// // Conver the data to an associative array
+$bookData = json_decode($jsonData, true);
 
-echo $jsonData;
+// Check that we received book information
+if ($bookData['numFound'] > 0) {
+    $book = $bookData['docs'][0];
+    
+    // Now you can access the 'data' key which contains the title
+    $title = $book['title'];
+} else {
+    echo "No records found for this ISBN.";
+} 
 
+echo $bookData['docs']['0']['title'] . PHP_EOL;
+
+// https://openlibrary.org/search.json?q=9781603020220
