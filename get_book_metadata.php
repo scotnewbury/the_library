afec1936ac99bfc0e822ec010f3d54b04326ee9c
Scot Newbury <scot@scotnewbury.com>
2026-03-03 16:01:21 -0500

    Successful build of API url and call

--- get_book_metadata.php
+++ get_book_metadata.php
@@ -2,7 +2,12 @@
 
 $isbn = "9781603020220";
 
-$url = "http://openlibrary.org/api/volumes/brief/isbn/". $isbn . ".json" . PHP_EOL;
+$url = "http://openlibrary.org/api/volumes/brief/isbn/". $isbn . ".json";
 
-echo $url;
+echo $url . PHP_EOL;
+
+$jsonData = file_get_contents($url);
+
+
+echo $jsonData;
 
