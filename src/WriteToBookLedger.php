90a4f69c0e87950bc470946ceeb78645ce7b7c5b
Scot Newbury <scot@scotnewbury.com>
2026-03-18 22:28:26 -0400

    Refactored WriteToBookLedger.php for OOP

---
+++ src/WriteToBookLedger.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * WriteToBookLedger.php
+ * Writes to the book.csv file the ISBN and current timestamp
+ */
+
+namespace Library;
+
+class WriteToBookLedger {
+  public function writeToBookLedger($bookInfo) {
+    $currentTimestamp = date('Y-m-d H:i:s', time());
+    // Create an absoloute path for the ledger
+    $ledgerPath = __DIR__ . '/../data/books.csv';
+    // Create the directory if it isn't there
+    if (!is_dir(__DIR__ . '/../data')) {
+      mkdir(__DIR__ . '/../data/', 0755, true);
+    }
+    $handle = fopen("$ledgerPath", "a");
+    if ($handle === false) {
+      die("Error: Unable to open the Book Ledger for writing.");
+    }
+    
+    // Pull just the array values out
+    $rowData = array_values($bookInfo);
+    // Add the current timestamp to the end of the row
+    $rowData[] = $currentTimestamp;
+    // Write the row
+    fputcsv($handle, $rowData);
+    fclose($handle);
+  }
+}
